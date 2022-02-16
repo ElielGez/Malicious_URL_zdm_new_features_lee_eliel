5da8473424a4d1b6589739a5a15a8f40330ac53b
Eliel Gez <eliel.gez@au10tix.com>
2022-02-16 17:11:01 +0200

    finish

--- Tools/Collect_SSL.py
+++ Tools/Collect_SSL.py
@@ -4,40 +4,44 @@ from urllib.parse import urlparse
 import requests
 import base64
 import json
+from datetime import datetime
 
 
 API_SECRET="8c1cf1a737c204b9"
 API_KEY="/XOX1muJq/3Wy9aKSQSutVFYFtl2XVTE"
+
 client = Client(API_SECRET, API_KEY)
 
-df= pd.read_csv('./LengthOfDomains.csv')
-domains= df['0']
+df= pd.read_csv('./newMalWithAds.csv')
+domains= df['domain']
 errors=0
 results=[]
 
 url_to_scan = ''
       
-url = f'https://api.riskiq.net/v1/ssl/cert/host?host={url_to_scan}'
-headers = {'Accept': 'application/json','Authorization': 'Basic OGMxY2YxYTczN2MyMDRiOTovWE9YMW11SnEvM1d5OWFLU1FTdXRWRllGdGwyWFZURQ=='}
+url = f'https://api.riskiq.net/pt/v2/ssl-certificate/history?query={url_to_scan}'
+headers = {'Accept': 'application/json','Authorization': 'Basic bGVla29jaGF2QGdtYWlsLmNvbToxNmE3Mzc1NmI1MzRmNGNkZmY4ZDc5NzYwZTIxMzYxYjNkZTQ3ZTJiMGQxOGMyMWZhNmVhMjRlMTFkMWM5MThl'}
 
 for index, domain in enumerate(domains):
-    new_domain = urlparse(domain).netloc
+    #new_domain = urlparse(domain).netloc
+    new_domain = domain
     print(f'Run {index}/{len(domains)}')
-    url = f'https://api.riskiq.net/v1/ssl/cert/host?host={new_domain}'
+    url = f'https://api.riskiq.net/pt/v2/ssl-certificate/history?query={new_domain}'
     res = requests.get(url, headers=headers)
     data =json.loads(res.text)
-    content = data['content']
+    content = data['results']
     final=0
     if content != [] :
-        first_seen= int(content[0]['firstSeen'])
-        last_seen = int (content[0]['lastSeen'])
+        first_seen= int(datetime.fromisoformat(content[0]['firstSeen']).timestamp())
+        last_seen = int (datetime.fromisoformat(content[0]['lastSeen']).timestamp())
         final=last_seen-first_seen
     results.append(final)
 
-df['6']=results
+#ssl_time
+df['ssl_time']=results
 df.drop('Unnamed: 0', inplace=True, axis=1)
 try:
-    df.to_csv('./CollectSSL.csv')
+    df.to_csv('./newMalWithAds.csv')
 except:
     textfile = open("CollectSSL.txt", "w")
     for element in results:
